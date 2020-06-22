cca8e0dcfa4ddb20da9eee380a5399ad2fa1fb44
Ximoromero6 <chimillo_95@hotmail.com>
2020-06-22 14:01:28 +0200

    Ficheros iniciales

---
+++ frontEnd/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegistroComponent } from './registro/registro.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegistroComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontEnd/src/app/registro/registro.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.scss']
+})
+export class RegistroComponent implements OnInit {
+
+  formularioRegistro: FormGroup
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.formularioRegistro = this.formBuilder.group({
+      nombre: ['fdfdf'],
+      usuario: ['fdfdf', Validators.compose([Validators.minLength(4), Validators.required])],
+      email: ['fdfdf@sss', Validators.compose([Validators.email, Validators.required])],
+      clave: ['fdfdfss', Validators.compose([Validators.minLength(6), Validators.required])]
+    });
+  }
+
+  validarDatosRegistro() {
+    if (this.formularioRegistro.invalid) {
+      this.formularioRegistro.get("nombre").markAsDirty();
+      this.formularioRegistro.get("usuario").markAsDirty();
+      this.formularioRegistro.get("email").markAsDirty();
+      this.formularioRegistro.get("clave").markAsDirty();
+      return;
+    }
+
+    alert(JSON.stringify(this.formularioRegistro.value));
+  }
+
+}
